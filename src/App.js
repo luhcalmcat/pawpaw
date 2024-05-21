9b6468addd2e86d5ddadff912b273ff034aa9c73
luhcalmcat <kkowalski1333@gmail.com>
2024-05-21 17:04:01 +0200

    homework

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import Header from './components/header/Header.js';
 import HomePage from './components/HomePage/HomePage.js';
-import Banner from './components/banner/Banner.js';
+import Footer from './components/Footer/Footer.js';
 
 function App() {
   return (
     <>
-      <Header />
-      <HomePage />
-      <Banner />  
+    <Header />
+    <HomePage />
+    <Footer />
     </>
   );
 }
